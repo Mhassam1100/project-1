c61846287d4d5d3dc09523f54441ee5057874004
Mhassam1100 <166863435+Mhassam1100@users.noreply.github.com>
2024-11-13 01:44:46 +0500

    Add files via upload

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-[#25423e]">
+        <h1 className="text-3xl font-bold text-white tracking-wide font-montserrat drop-shadow-lg">
+         <b> Welcome to the My Contact Page - 03153038059</b>
+        </h1>
+      </div>
+    );
+  }
---
+++ src/app/services/page.tsx
@@ -0,0 +1,10 @@
+export default function Services() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-[#6e532b]">
+        <h1 className="text-4xl font-bold text-white tracking-wide font-montserrat drop-shadow-lg">
+          Our Services Plans
+        </h1>
+      </div>
+    );
+  }
+  
